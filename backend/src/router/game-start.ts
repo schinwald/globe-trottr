89494a0d6af2d7240c131dc99fb0fda4a50634ab
schinwald <jamesschinwald@gmail.com>
2025-10-31 17:07:10 -0400

    feat: publish game country status when game starts

--- backend/src/router/game-start.ts
+++ backend/src/router/game-start.ts
@@ -1,5 +1,10 @@
 import z from "zod";
-import { publishGameStatus, startGame } from "../utils/redis-schema.js";
+import { countries } from "../utils/countries.js";
+import {
+	publishGameCountryStatus,
+	publishGameStatus,
+	startGame,
+} from "../utils/redis-schema.js";
 import { t } from "../utils/trpc.js";
 
 export const procedure = t.procedure
@@ -16,4 +21,14 @@ export const procedure = t.procedure
 			startedAt,
 		});
 		ctx.log.info({ startedAt }, "Publishing game started");
+
+		await publishGameCountryStatus(input.roomCode, "country-statuses", {
+			countries: countries.map((country) => {
+				return {
+					...country,
+					guessed: false,
+				};
+			}),
+		});
+		ctx.log.info({ startedAt }, "Publishing game country started");
 	});
