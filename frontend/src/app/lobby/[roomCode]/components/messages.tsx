e6ff0992df363f43c65a20add9d0c0341d03fa53
schinwald <jamesschinwald@gmail.com>
2025-12-30 19:30:43 -0500

    feat: fix message timeout overlapping

--- frontend/src/app/lobby/[roomCode]/components/messages.tsx
+++ frontend/src/app/lobby/[roomCode]/components/messages.tsx
@@ -1,3 +1,4 @@
+import { dateFromTimestamp } from "@globe-trottr/shared/utils/protobuf.js"
 import * as PrimitivePortal from "@radix-ui/react-portal"
 import { AnimatePresence, motion } from "framer-motion"
 import {
@@ -42,6 +43,7 @@ export const Notification: React.FC<PortalProps> = ({
   ...props
 }) => {
   const { ref } = useContext(NotificationContext)
+  const timeoutRefs = useRef<Record<string, NodeJS.Timeout>>({})
   const { messages, dismissMessage } = useGameStore(
     useShallow((state) => ({
       messages: state.messages,
@@ -49,48 +51,59 @@ export const Notification: React.FC<PortalProps> = ({
     }))
   )
 
+  const userMessages = Object.entries(messages).filter(([_, data]) => {
+    return data.userId === userId
+  })
+
   return (
     <AnimatePresence mode="popLayout">
-      {Object.entries(messages)
-        .filter(([_, data]) => {
-          return data.userId === userId
-        })
-        .map(([id, data]) => (
-          <PrimitivePortal.Root
-            key={id}
-            container={ref?.current}
-            className={cn(
-              "pointer-events-none absolute left-[100%] top-[50%] -translate-y-[50%] text-foreground z-40",
-              className
-            )}
-            {...props}
+      {userMessages.map(([_, data]) => (
+        <PrimitivePortal.Root
+          key={data.id}
+          container={ref?.current}
+          className={cn(
+            "pointer-events-none absolute left-[100%] top-[50%] -translate-y-[50%] text-foreground z-40",
+            className
+          )}
+          {...props}
+        >
+          <motion.div
+            className="origin-left"
+            initial={{ opacity: 0, translateX: "-10px", scale: 0.7 }}
+            animate={{ opacity: 1, translateX: "0px", scale: 1 }}
+            exit={{ opacity: 0, scale: 0.8 }}
+            transition={{
+              duration: 0.1,
+              onPlay: () => {
+                for (const [key, value] of Object.entries(
+                  timeoutRefs.current
+                )) {
+                  clearTimeout(value)
+                  delete timeoutRefs.current[key]
+                  dismissMessage(key)
+                }
+              },
+              onComplete: () => {
+                const timeoutId = setTimeout(
+                  () => {
+                    dismissMessage(data.id)
+                  },
+                  1500 + data.message.length * 50
+                )
+
+                timeoutRefs.current[data.id] = timeoutId
+              },
+            }}
           >
-            <motion.div
-              className="origin-left"
-              initial={{ opacity: 0, translateX: "-10px", scale: 0.7 }}
-              animate={{ opacity: 1, translateX: "0px", scale: 1 }}
-              exit={{ opacity: 0, scale: 0.8 }}
-              transition={{
-                duration: 0.1,
-                onComplete: () => {
-                  setTimeout(
-                    () => {
-                      dismissMessage(id)
-                    },
-                    1500 + data.message.length * 50
-                  )
-                },
-              }}
-            >
-              <div className="flex flex-row items-center ml-2">
-                <ArrowLeftIcon className="size-5 -m-2 text-primary" />
-                <div className="rounded-full px-4 py-2 bg-primary text-white border-gray-300 whitespace-nowrap">
-                  {data.message}
-                </div>
+            <div className="flex flex-row items-center ml-2">
+              <ArrowLeftIcon className="size-5 -m-2 text-primary" />
+              <div className="rounded-full px-4 py-2 bg-primary text-white border-gray-300 whitespace-nowrap">
+                {data.message}
               </div>
-            </motion.div>
-          </PrimitivePortal.Root>
-        ))}
+            </div>
+          </motion.div>
+        </PrimitivePortal.Root>
+      ))}
     </AnimatePresence>
   )
 }
